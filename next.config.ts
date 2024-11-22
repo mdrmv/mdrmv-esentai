a88c9652b0a36abcdac282c1a90e4f299ec49f5e
Marat Madraimov <mmadraimov@ngdem.io>
2024-11-22 14:46:03 +0500

    f1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from 'next';
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/mdrmv-esentai',
+  assetPrefix: '/mdrmv-esentai',
 };
 
 export default nextConfig;
